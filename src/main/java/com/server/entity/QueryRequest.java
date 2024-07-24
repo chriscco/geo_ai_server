f1776093d361627d5e3797eb2d69ef4895912e99
chriscco <2444628489c@gmail.com>
2024-07-24 15:52:41 +0800

    initial commit

---
+++ src/main/java/com/server/entity/QueryRequest.java
@@ -0,0 +1,11 @@
+package com.server.entity;
+
+public class QueryRequest {
+    private String query;
+    public String getQuery() {
+        return query;
+    }
+    public void setQuery(String query) {
+        this.query = query;
+    }
+}
