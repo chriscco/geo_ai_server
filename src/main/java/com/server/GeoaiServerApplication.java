e3d6c7d92f22209ed402c90c257aec1154da1367
chriscco <2444628489c@gmail.com>
2024-07-24 14:29:32 +0800

    first commit

---
+++ src/main/java/com/server/GeoaiServerApplication.java
@@ -0,0 +1,27 @@
+package com.server;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class GeoaiServerApplication {
+    @Bean
+    WebMvcConfigurer createWebMvcConfigurer(@Autowired HandlerInterceptor[] interceptors) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").
+                        addResourceLocations("classpath:/static/");
+            }
+        };
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(GeoaiServerApplication.class, args);
+    }
+
+}
