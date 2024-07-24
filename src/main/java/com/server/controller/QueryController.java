3ed1d29e0ce7147db849ae0d4bd6dbb5310ed80e
chriscco <2444628489c@gmail.com>
2024-07-24 16:27:05 +0800

    call python api

--- src/main/java/com/server/controller/QueryController.java
+++ src/main/java/com/server/controller/QueryController.java
@@ -1,14 +1,43 @@
 package com.server.controller;
 
+import com.server.PythonCaller;
+import com.server.entity.QueryRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@RestController
+import java.io.IOException;
+
+@Controller
+@PropertySource(value = "classpath:config.properties")
 public class QueryController {
+    private Environment env;
+    private static String interpreter_path = null;
+    private static String script_path = null;
 
-    @GetMapping(value = "/ai")
+    @Autowired
+    public void setEnv(Environment env) {
+        this.env = env;
+    }
+    @GetMapping("/ai")
     public String getIndex() {
+        interpreter_path = env.getProperty("path.interpreter_path");
+        script_path = env.getProperty("path.script_path");
         return "index";
     }
-
+    @PostMapping("/ai/query")
+    public String query(@RequestBody QueryRequest request)
+            throws IOException, InterruptedException {
+        PythonCaller pythonCaller = new PythonCaller();
+        String query = request.getQuery();
+        String[] result = pythonCaller.call(interpreter_path, script_path, query);
+        return null;
+    }
 }
+
+// CITE:
+// https://github.com/OujaKivi/upload-callpython-springboot/blob/master/src/main/java/com/wesley/uploadingfiles/UploadingController.java
