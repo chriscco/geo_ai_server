03ef4ee8962cd212604a8f261a714698719a0f5f
chriscco <2444628489c@gmail.com>
2024-08-01 16:06:27 +0800

    commented

--- src/main/java/com/server/controller/QueryController.java
+++ src/main/java/com/server/controller/QueryController.java
@@ -8,6 +8,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.io.File;
 import java.io.IOException;
@@ -32,8 +33,8 @@ public class QueryController {
      * @return in json
      */
     @GetMapping("/ai")
-    public ResponseEntity<String> getIndex() {
-        return ResponseEntity.ok("This is home page");
+    public ModelAndView getIndex() {
+        return new ModelAndView("index");
     }
 
     /**
@@ -69,6 +70,7 @@ public class QueryController {
         String upload_file_path = env.getProperty("path.upload_file_path");
         assert upload_file_path != null;
         File dir = new File(upload_file_path);
+        // save file to where RAG model reads documents, rename the file
         file.transferTo(new File(dir + File.separator + "upload.pdf"));
         return ResponseEntity.ok("Upload Success!");
     }
